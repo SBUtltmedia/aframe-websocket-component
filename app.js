b043bb11e271543d77e1eedb71e411f4f132f367
TLT Media Lab <tltmedialab@connect.stonybrook.edu>
2019-01-14 13:45:15 -0500

    Add files via upload
    Added base files

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/examples/basic/index.html');
+});
+
+app.get('/controller', function (req, res) {
+  res.sendFile(__dirname + '/examples/basic/aframeComponentControl.html');
+});
+// app.get('/dist/socket.io.js', function (req, res) {
+//   res.sendFile(__dirname + '/dist/socket.io.js');
+// });
+app.use('/dist', express.static('dist'))
+io.on('connection', function (socket) {
+
+  socket.on('controlComponent', function (data) {
+    console.log('Received Control')
+      io.emit('updateComponent', data);
+  });
+});
