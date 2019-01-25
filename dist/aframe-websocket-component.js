4afc6e9d0d47cb6eb4e813986f313a929c46c9cc
bazal1 <dadsibasel@gmail.com>
2019-01-25 12:15:16 -0500

    Fixed rotation

--- dist/aframe-websocket-component.js
+++ dist/aframe-websocket-component.js
@@ -97,140 +97,110 @@ AFRAME.registerComponent('websocket', {
    * Called once when component is attached. Generally for initial setup.
    */
   init: function(evt) {
+    this.sendList = {};
     this.deltaT = 0;
-    this.sendToTick;
-    this.attributeList = {};
+    // this.attributeList = {};
     this.socket = io.connect('http://localhost');
-    console.log(this.el.components);
+
     if (this.data.userType == "client") {
       this.socket.on('updateComponents', (attributeList) => {
 
-          this.sendToTick = attributeList;
+        if (this.data.userType == "client") {
 
+          for (i in attributeList) {
+            var currentAttribute = attributeList[i];
+
+            // var currentRotation = this.el.getAttribute('rotation');
+            // if (i == "rotation") {
+            //   if (currentRotation != currentAttribute) {
+            //     this.el.setAttribute(i, currentAttribute);
+            //   }
+            // } else {
+
+
+            // currentAttribute = currentAttribute || {};
+            //     console.log(i,j, currentAttribute[j])
+            this.el.setAttribute(i, currentAttribute);
+
+          }
+
+        }
       })
-  }
+    }
 
-},
+  },
 
-/**
- * Called when component is attached and when component data changes.
- * Generally modifies the entity based on the data.
- */
-update: function(oldData) {
-  console.log("websocket is updating");
-},
+  /**
+   * Called when component is attached and when component data changes.
+   * Generally modifies the entity based on the data.
+   */
+  update: function(oldData) {
+    console.log(oldData);
+  },
 
-/**
- * Called when a component is removed (e.g., via removeAttribute).
- * Generally undoes all modifications to the entity.
- */
-remove: function() {},
+  /**
+   * Called when a component is removed (e.g., via removeAttribute).
+   * Generally undoes all modifications to the entity.
+   */
+  remove: function() {},
 
-/**
- * Called on each scene tick.
- */
-tick: function(t) {
-  if (this.data.userType == "controller") {
-    if (t > this.deltaT + 1000) {
-      this.deltaT = t;
-      var needsChange = false;
-      var sendList = {};
-      for (i of this.el.attributes) {
-        //var propArr=[];
-        if (i.name != this.attrName && i.name != "id") {
-          //var attributeProperties = {};
-          var currentAttributeProps = this.el.getAttribute(i.name);
-          for (j in currentAttributeProps) {
-            this.attributeList[i.name] = this.attributeList[i.name] || {};
-            if (Object.values(this.attributeList).length == 0 || this.attributeList[i.name][j] != currentAttributeProps[j]) {
-              sendList[i.name] = sendList[i.name] || {};
-              sendList[i.name][j] = currentAttributeProps[j];
-              if (i.name == 'rotation') {
-                if (sendList["rotation"].x != undefined && sendList["rotation"].y != undefined) {
-                  needsChange = true;
+  /**
+   * Called on each scene tick.
+   */
+  tick: function(t) {
+
+    if (this.data.userType == "controller") {
+      if (t > this.deltaT + 10) {
+        this.deltaT = t;
+        var needsChange = false;
+
+        for (i of this.el.attributes) {
+          console.log(i)
+          if (i.name != this.attrName && i.name != "id") {
+            //var attributeProperties = {};
+            var currentAttributeProps = this.el.getAttribute(i.name);
+            this.sendList[i.name] = this.sendList[i.name] || {}
+
+            for (j in currentAttributeProps) {
+
+              if (this.sendList[i.name][j] !== currentAttributeProps[j]) {
+                if (typeof currentAttributeProps[j] != "function") {
+                  this.sendList[i.name][j] = currentAttributeProps[j];
                 }
-              } else {
+                // if (i.name == 'rotation') {
+                //   if (sendList["rotation"].x != undefined && sendList["rotation"].y != undefined) {
+                //     needsChange = true;
+                //   }
+                // } else {
                 needsChange = true;
+                // }
               }
             }
           }
         }
-      }
-      // var sendList = {"rotation":{}};
-      // sendList["rotation"].x= this.el.getAttribute("rotation").x;
-      // sendList["rotation"].y= this.el.getAttribute("rotation").y;
-      // sendList["rotation"].z= this.el.getAttribute("rotation").z;
-      if (needsChange) {
-        this.socket.emit('controlComponent', sendList);
+        if (needsChange) {
+          this.socket.emit('controlComponent', this.sendList);
 
-        for (i in sendList) {
-          var currentVar = sendList[i];
-          this.attributeList[i] = sendList[i];
+          for (i in this.sendList) {
+            var currentVar = this.sendList[i];
+          }
         }
-        // this.attributeList = JSON.parse(JSON.stringify(sendList))
-        //this.attributeList = Object.assign(sendList, this.attributeList);
       }
     }
 
+  },
 
-    // if(!(i.nodeName in this.attributeList) || this.attributeList[i.nodeName] != i.nodeValue){
-    //   sendList[i.nodeName] = i.nodeValue;
-    //   //console.log(this.el.attributes)
-    //   // if(i.name == 'glmol'){
-    //   //   for(j in i){
-    //   //     console.log(j);
-    //   //   }
-    //   // }
-    //   // console.log((this.attributeList[i.name]) === this.el.getAttribute(i.name))
-    //   //
-    //   // this.attributeList[i.name] = this.el.getAttribute(i.name)
-    //   // console.log(this.el.getAttribute(i.name));
-    // }
-  }
-  // if(Object.keys(sendList).length != 0){
-  //   for(i of Object.keys(sendList)){
-  //     //console.log(i, sendList[i]);
-  //     this.attributeList[i] = sendList[i];
-  //     //Object.assign({},this.attributeList, {i: "value3"});
-  //     console.log(this.attributeList)
-  //     //this.el.glmol.setAttribute(i, this.attributeList[i]);
-  //   }
-  //   //this.el.glmol.update();
-  // }
-  //console.log(this.attributeList);
+  /**
+   * Called when entity pauses.
+   * Use to stop or remove any dynamic or background behavior such as events.
+   */
+  pause: function() {},
 
-
-  if (this.data.userType == "client") {
-
-    for (i in this.sendToTick) {
-
-      var currentAttribute = this.sendToTick[i];
-      if (i == "rotation") {
-      //  console.log(currentAttribute)
-        this.el.setAttribute(i, currentAttribute);
-        //console.log(this.el.getAttribute(i));
-        // console.log(i,currentAttribute)
-      } else {
-        // for (j in currentAttribute) {
-        //   var currentProperty = currentAttribute[j]
-        //   this.el.setAttribute(i, j, currentProperty)
-        // }
-      }
-    }
-  }
-},
-
-/**
- * Called when entity pauses.
- * Use to stop or remove any dynamic or background behavior such as events.
- */
-pause: function() {},
-
-/**
- * Called when entity resumes.
- * Use to continue or add any dynamic or background behavior such as events.
- */
-play: function() {}
+  /**
+   * Called when entity resumes.
+   * Use to continue or add any dynamic or background behavior such as events.
+   */
+  play: function() {}
 });
 
 AFRAME.registerComponent('drag-rotate-component', {
